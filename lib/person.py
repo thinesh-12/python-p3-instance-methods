74328c607cc1a49b4263c0c73adafc6fe95c085d
thinesh <thinesh12@gmail.com>
2023-07-23 10:37:38 -0400

    Update person.py

--- lib/person.py
+++ lib/person.py
@@ -5,7 +5,7 @@ class Person:
 
     #Instance method definition
     def talk(self):
-           print("hello World!")
+           print("Hello World!")
            
     def walk(self):
         print("The person is walking.")
